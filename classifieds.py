a7d45d8290e14ee88f4e15af61a346bee470ad4b
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-08 15:00:09 +0000

    amended minor details

--- classifieds.py
+++ classifieds.py
@@ -88,4 +88,4 @@ def load_ads():
         
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
