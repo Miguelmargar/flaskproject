07fe3eb2eb327aa130045871a318ffe3d16edbd4
miguelmargar <miguelmargar@outlook.com>
2021-11-05 14:27:35 +0000

    added PEP8 double lines to tests.py and classifieds.py

--- classifieds.py
+++ classifieds.py
@@ -10,47 +10,51 @@ MONGODB_NAME = os.environ.get("MONGODB_NAME")
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index_page():
     loaded_ads = load_ads()
     return render_template("index.html", ads_front=loaded_ads)
- 
+
+
 @app.route("/create")
-def get_create_ad():    
+def get_create_ad():
     return render_template("createad.html")
- 
+
+
 @app.route("/howitworks")
 def howitworks():
     return render_template("howworks.html")
-    
+
+
 @app.route("/contactus")
 def contactus():
     return render_template("contactus.html")
-   
-@app.route("/handle_create", methods=["POST"]) 
-def create_ad():
 
-    price       = request.form.get("price")
-    make        = request.form.get("make")
-    model       = request.form.get("model")
-    year        = request.form.get("year")
-    odometer    = request.form.get("odometer")
-    color       = request.form.get("color")
-    location    = request.form.get("location")
-    email       = request.form.get("email")
-    phone       = request.form.get("phone")
-    file        = request.files.get("image")
-    
+
+@app.route("/handle_create", methods=["POST"])
+def create_ad():
+    price = request.form.get("price")
+    make = request.form.get("make")
+    model = request.form.get("model")
+    year = request.form.get("year")
+    odometer = request.form.get("odometer")
+    color = request.form.get("color")
+    location = request.form.get("location")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    file = request.files.get("image")
+
     f = "static/" + os.path.join(os.path.basename("uploads"), file.filename)
-    
+
     main_path = f[:f.find(".")]
     extension = f[f.find("."):]
     random_mix = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    
+
     if os.path.exists(f):
         f = main_path + "_" + random_mix + extension
     file.save(f)
-    
+
     ad = {
         "price": price,
         "make": make,
@@ -61,20 +65,21 @@ def create_ad():
         "location": location,
         "email": email,
         "phone": phone,
-        "image_path": f 
+        "image_path": f
     }
-    
+
     save_to_mongo(ad)
-    
+
     return redirect("/")
 
 
 def save_to_mongo(ad):
     with MongoClient(MONGODB_URI) as conn:
-       db = conn[MONGODB_NAME]
-       coll = db["cars"]
-       coll.insert(ad)
-        
+        db = conn[MONGODB_NAME]
+        coll = db["cars"]
+        coll.insert(ad)
+
+
 def load_ads():
     with MongoClient(MONGODB_URI) as conn:
         db = conn[MONGODB_NAME]
@@ -84,8 +89,7 @@ def load_ads():
         for i in carad:
             car_list.append(i)
         return car_list
-            
-        
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
--- tests.py
+++ tests.py
@@ -5,26 +5,28 @@ from classifieds import *
 
 TEST_DB = 'test.db'
 
+
 class Basic_url_tests(TestCase):
-    
+
     def setUp(self):
         self.app = app.test_client()
-   
+
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-       
+
     def test_howitworks_page(self):
         response = self.app.get('/howitworks', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)  
-       
+        self.assertEqual(response.status_code, 200)
+
     def test_create_page(self):
         response = self.app.get('/create', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)   
-        
+        self.assertEqual(response.status_code, 200)
+
     def test_contactus_page(self):
         response = self.app.get('/contactus', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)   
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
